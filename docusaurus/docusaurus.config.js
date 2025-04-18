d5bd7ce970acedea441d44d9662ad2efb544c3ec
keyestudio <215650023@qq.com>
2025-04-18 16:27:50 +0800

    2025-04-18 16:27:50.17

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -145,6 +145,21 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      
+      
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        hashed: true,
+      
+      }),
+      
+      
+      
+      
+      
+      
+      
     }),
 };
 
